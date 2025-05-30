9147eb3204bb28414ebc0b7b91c9c0e723550929
Belén M <100541119+bemartin94@users.noreply.github.com>
2025-05-30 19:03:15 -0300

    changes in models

--- core/models.py
+++ core/models.py
@@ -64,8 +64,8 @@ class IdCardType(Base):
     enabled_visitors = relationship("EnabledVisitor", back_populates="id_card_type")
     denied_visitors = relationship("DeniedVisitor", back_populates="id_card_type")
 
-class EnabledVisitor(Base):
-    __tablename__ = "enabled_visitor"
+class Visitor(Base):
+    __tablename__ = "visitors"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
@@ -82,26 +82,6 @@ class EnabledVisitor(Base):
     id_card_type = relationship("IdCardType", back_populates="enabled_visitors")
 
 
-
-"""pensamos como alternativa eliminar las clases deniedvisitor y accessdenied,
- que la clase se llame simplemente Visitor
-y agregar un campo en la clase Visitor que sea is_enabled. Lo mismo para access"""
-class DeniedVisitor(Base):
-    __tablename__ = "denied_visitor"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=True)
-    last_name = Column(String, nullable=True)
-    id_card = Column(String, unique=True, index=True)
-    phone = Column(String, nullable=True)
-    email = Column(String, unique=True, index=True)
-    picture = Column(String, nullable=True)
-
-    supervisor_id = Column(Integer, ForeignKey("supervisor.id"))
-    venue_id = Column(Integer, ForeignKey("venues.id"))
-    id_card_type_id = Column(Integer, ForeignKey("id_card_types.id"))
-
-    id_card_type = relationship("IdCardType", back_populates="denied_visitors")
-
 class AccessTime(Base):
     __tablename__ = "access_times"
     id = Column(Integer, primary_key=True, index=True)
@@ -112,44 +92,21 @@ class AccessTime(Base):
     access_enabled = relationship("AccessEnabled", back_populates="access_time")
 
 
-class AccessEnabled(Base):
-    __tablename__ = "access_enabled"
+class Access(Base):
+    __tablename__ = "accesses"
     id = Column(Integer, primary_key=True, index=True)
     venue_id = Column(Integer, ForeignKey("venues.id"))
+    visitor_id = Column(Integer, ForeignKey("visitors.id"), nullable=True)
     id_card_type_id = Column(Integer, ForeignKey("id_card_types.id"))
     id_card_id = Column(Integer, ForeignKey("enabled_visitor.id"))
     id_supervisor = Column(Integer, ForeignKey("supervisor.id"))
     
     access_reason = Column(String, nullable=True)
-    is_recurrent = Column(Boolean, nullable=True)
-    access_time = relationship("AccessTime", uselist=False, back_populates="access_enabled")
-
-    id_card_type = relationship("IdCardType")
-    enabled_visitor = relationship("EnabledVisitor")
-
-
-class AccessDenied(Base):
-    __tablename__ = "access_denied"
-    id = Column(Integer, primary_key=True, index=True)
-    venue_id = Column(Integer, ForeignKey("venues.id"))
-    id_card_type_id = Column(Integer, ForeignKey("id_card_types.id"))
-    id_card_id = Column(Integer, ForeignKey("denied_visitor.id"))
-    id_supervisor = Column(Integer, ForeignKey("supervisor.id"))
-    
-    access_reason = Column(String, nullable=True)
     department = Column(String, nullable=True)
     is_recurrent = Column(Boolean, nullable=True)
+    status = Column(String, nullable=False)
 
-    access_time = relationship("AccessDeniedTime", uselist=False, back_populates="access_denied")
+    access_time = relationship("AccessTime", uselist=False, back_populates="access_enabled")
     id_card_type = relationship("IdCardType")
-    denied_visitor = relationship("DeniedVisitor")
+    visitor = relationship("Visitor")
 
-
-class AccessDeniedTime(Base):
-    __tablename__ = "access_denied_times"
-    id = Column(Integer, primary_key=True, index=True)
-    access_date = Column(DateTime, nullable=True)
-    exit_date = Column(DateTime, nullable=True)
-
-    access_denied_id = Column(Integer, ForeignKey("access_denied.id"))
-    access_denied = relationship("AccessDenied", back_populates="access_time")
